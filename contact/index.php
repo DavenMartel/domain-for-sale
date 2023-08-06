5aba0cf39a71031319fb381152a2fad3c49da422
DavenMartel <info@davenmartel.com>
2023-08-05 21:05:33 -0400

    Personalisation

---
+++ contact/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+		<head>
+				<meta charset="utf-8">
+				<title>Sales Inquery || <?=$_SERVER['SERVER_NAME']?></title>
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css">
+				<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+				<link href="https://fonts.googleapis.com/css?family=Mukta+Mahee:300,700" rel="stylesheet">
+				<link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css" />
+				<link rel="stylesheet" href="css/style.css" />
+		</head>
+		<body>
+			<section class="bg-alt hero p-0">
+				<div class="container-fluid">
+					<div class="row">
+							<div class="bg-faded col-sm-6 text-center col-fixed">
+									<div class="vMiddle">
+										<h1 class="pt-4 h2">
+											<span>Thank you for your offer, we will reach out to you as soon as possible.</span>
+										</h1>
+										<div class="row d-md-flex text-center justify-content-center text-primary action-icons">
+											<div class="col-sm-4">
+												<p><em class="ion-ios-chatbubble-outline icon-md"></em></p>
+												<p class="lead"><a href="mailto:buy@<?=$_SERVER['SERVER_NAME']?>">buy@<?=$_SERVER['SERVER_NAME']?></a></p>
+											</div>
+									</div>
+								</div>
+							</div>
+							<div class="col-sm-6 offset-sm-6 px-0">
+									<section class="bg-alt">
+											<div class="row height-100">
+													<div class="col-sm-8 offset-sm-2 mt-2">
+														<h1 class="pt-4 h2"><span class="text-green">— Daven Martel</span></h1>
+														<span class="text-muted">Tech Consultant</span>
+														<p><span>QC, Canada</span></p>
+														<br/>
+														<a target="_blank" href="https://davenmartel.com">davenmartel.com</a>
+													</div>
+											</div>
+									</section>
+							</div>
+					</div>
+				</div>
+			</section>
+		</body>
+</html>
--- index.php
+++ index.php
@@ -2,15 +2,13 @@
 <html lang="en">
     <head>
         <meta charset="utf-8">
-        <title>Sales Inquery || [Your Domain]</title>
+        <title>Sales Inquery || <?=$_SERVER['SERVER_NAME']?></title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
         <link href="https://fonts.googleapis.com/css?family=Mukta+Mahee:300,700" rel="stylesheet">
         <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css" />
         <link rel="stylesheet" href="css/style.css" />
-        <script src="https://www.google.com/recaptcha/api.js" async defer></script>
-
     </head>
     <body>
 
@@ -20,25 +18,21 @@
                     <div class="col-sm-6 bg-faded text-center col-fixed">
                         <div class="vMiddle">
                           <h1 class="pt-4 h2">
-                              <span class="text-green">[Your Domain].com</span>
-                              <small>available for sale</small>
+                              <span class="text-green"><?=$_SERVER['SERVER_NAME']?></span>
+                              <small>is available for sale</small>
                           </h1>
                           <p class="mt-4">
-                              For instantly purchase. Please make an order.
+                              Instant purchase available.
                           </p>
                           <div class="pt-5">
                               <label for="name">
-                              <a class="btn text-white bg-green btn-lg">Buy now for $4999</a>
+                              <a class="btn text-white bg-green btn-lg" href="https://buy.stripe.com/00gfZMd7WgF72aY3cc">Buy now for $11 000</a>
                               </label>
                           </div>
                           <div class="row d-md-flex text-center justify-content-center text-primary action-icons">
                               <div class="col-sm-4">
-                                  <p><em class="ion-ios-telephone-outline icon-md"></em></p>
-                                  <p class="lead"><a href="tel:+[Your Phone]">+[Your Phone]</a></p>
-                              </div>
-                              <div class="col-sm-4">
                                   <p><em class="ion-ios-chatbubble-outline icon-md"></em></p>
-                                  <p class="lead"><a href="mailto:email@[Your Domain].com">email@[Your Domain].com</a></p>
+                                  <p class="lead"><a href="mailto:buy@<?=$_SERVER['SERVER_NAME']?>">buy@<?=$_SERVER['SERVER_NAME']?></a></p>
                               </div>
                           </div>
                       </div>
@@ -48,16 +42,22 @@
                             <div class="container">
                                 <div class="row height-100">
                                     <div class="col-sm-10 offset-sm-1 mt-2">
-                                        <form id="main-offer-form" action="contact.php" method="post">
+                                        <form id="main-offer-form" action="https://formspree.io/f/xwkdvdnl" method="post">
                                             <h2 class="text-primary">Interested in this domain?</h2>
                                             <hr/>
+                                            <input
+                                                  type="hidden"
+                                                  name="domain"
+                                                  id="domain"
+                                                  value="<?=$_SERVER['SERVER_NAME']?>"
+                                                >
                                             <div class="form-group">
                                                 <input
                                                   type="text"
                                                   name="name"
                                                   id="name"
                                                   class="form-control"
-                                                  placeholder="Full name (Required)"
+                                                  placeholder="Full name*"
                                                 >
                                             </div>
                                             <div class="row">
@@ -67,7 +67,7 @@
                                                       type="email"
                                                       name="email"
                                                       class="form-control"
-                                                      placeholder="Email (Required)"
+                                                      placeholder="Email*"
                                                     >
                                                 </div>
                                               </div>
@@ -77,7 +77,7 @@
                                                       type="text"
                                                       name="phone"
                                                       class="form-control"
-                                                      placeholder="Phone number (Required)"
+                                                      placeholder="Phone number*"
                                                     >
                                                 </div>
                                               </div>
@@ -88,13 +88,10 @@
                                                   name="price"
                                                   class="form-control"
                                                   min="0"
-                                                  placeholder="Offer price in USD? (Required)">
+                                                  placeholder="Offer (USD)?*">
                                             </div>
                                             <div class="form-group">
                                                 <textarea name="comments" class="form-control" placeholder="Message (optional)"></textarea>
-                                            </div>
-                                            <div class="form-group">
-                                              <div id="recaptcha" class="g-recaptcha" data-sitekey="6Lc_sf8aAAAAAFk3KIcYvqcIoo1HeCOZu4faK3DA" data-callback="onSubmit" data-size="invisible"></div>
                                             </div>
 
                                             <button type="submit" class="btn text-white btn-lg bg-primary btn-block">Make an offer</button>
@@ -138,17 +135,17 @@
               messages: {
                 name: 'Please enter your name.',
                 email: {
-                  required: 'You can not leave this empty.',
+                  required: 'Please enter your email.',
                   email: 'Please enter a valid email address.'
                 },
                 phone: {
-                  required: 'You can not leave this empty.',
+                  required: 'Please enter your phone number.',
                   matches: 'Please enter a valide phone number.',
                   minlength: 'Phone number should be min 10 digits.',
                   maxlength: 'Phone number should be max 10 digits.'
                 },
                 price: {
-                  price: 'Please enter offered price.'
+                  required: 'Please enter your offered amount.'
                 },
                 comments: {
                   maxlength: 'Message length must be less than 500 character.'
